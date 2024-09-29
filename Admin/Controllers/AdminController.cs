1b150d500bfad40845f63f74c74432c481fb5fd5
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-09-29 13:39:39 +0300

    AnasayfaGetir And İkons

--- Admin/Controllers/AdminController.cs
+++ Admin/Controllers/AdminController.cs
@@ -18,5 +18,32 @@ namespace Admin.Controllers
             var deger = _context.Anasayfas.ToList();
             return View(deger);
         }
+
+        public IActionResult AnaSayfaGetir(int Id)
+        {
+            var ag = _context.Anasayfas.Find(Id);
+            return View("AnaSayfaGetir", ag);
+        }
+
+        public IActionResult AnaSayfaGüncelle(Anasayfa x)
+        {
+
+            var ag = _context.Anasayfas.Find(x.Id);
+            ag.Profil = x.Profil;
+            ag.İsim = x.İsim;
+            ag.Unvan = x.Unvan;
+            ag.Aciklama = x.Aciklama;
+            ag.İletisim = x.İletisim;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult İkonListesi() 
+        {
+            var deger = _context.İkons.ToList();
+            return View(deger);
+        }
+
     }
 }
